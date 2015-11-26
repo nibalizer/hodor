3303effd7f806e832fc86c8bf2972f6c14394b65
Spencer Krum <nibz@cat.pdx.edu>
2015-11-26 10:39:11 -0800

    Hodor v2

---
+++ README.md
@@ -0,0 +1,27 @@
+Hodor
+=====
+
+
+Hodor doesn't speak much english, the only thing he can do is make you a vm. Hodor doesn't care
+
+
+Usage:
+
+hodor.py
+
+
+
+History
+=======
+
+
+This grew out of the hodor shell script in nibalizer's dotfiles: https://github.com/nibalizer/dotfiles/blob/master/local/bin/hodor
+
+
+
+Installation
+============
+
+
+
+Hodor will probably always require shade from git.
---
+++ hodor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+import os
+import shade
+import uuid
+
+
+
+if __name__ == "__main__":
+
+    shade.simple_logging(debug=True)
+
+    user = os.getlogin()
+
+    #cloud = shade.openstack_cloud(cloud='nibz-vexxhost')
+    cloud = shade.openstack_cloud()
+
+    flavor = cloud.get_flavor_by_ram(512)
+
+    image = cloud.get_image('Ubuntu 14.04.3 LTS')
+
+    key = cloud.search_keypairs(name_or_id=user)
+
+    server_name = "hodor-" + str(uuid.uuid4())
+
+    #cloud.create_server( server_name, image['id'], flavor['id'], key_name=key[0]['id'])
+
+
